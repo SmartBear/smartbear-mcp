02a3ea228a17d514a2da12bd601213e1c2c952a2
Assis <assisblf@gmail.com>
2025-10-03 16:17:34 -0300

    TM4J-13478 Extract tool logic and description to dedicated class

--- src/zephyr/client.ts
+++ src/zephyr/client.ts
@@ -1,61 +1,38 @@
-import type { Client, GetInputFunction, RegisterToolsFunction } from "../common/types.js";
+import type {
+  Client,
+  GetInputFunction,
+  RegisterToolsFunction,
+} from "../common/types.js";
 import { ApiClient } from "./common/api-client.js";
-import z from "zod";
-import { ProjectTools } from "./tool/project.js";
+import { GetProjects } from "./tool/get-projects.js";
+import type { ZephyrTool } from "./tool/zephyr-tool.js";
 
 export class ZephyrClient implements Client {
-  private readonly projectTools: ProjectTools;
+  private readonly apiClient: ApiClient;
   name = "Zephyr Test Management for Jira";
   prefix = "zephyr";
 
-  constructor(bearerToken: string,
-              baseUrl: string = 'https://api.zephyrscale.smartbear.com/v2') {
-    const apiClient = new ApiClient(bearerToken, baseUrl);
-    this.projectTools = new ProjectTools(apiClient);
+  constructor(
+    bearerToken: string,
+    baseUrl: string = "https://api.zephyrscale.smartbear.com/v2",
+  ) {
+    this.apiClient = new ApiClient(bearerToken, baseUrl);
   }
 
-  registerTools(register: RegisterToolsFunction, _getInput: GetInputFunction): void {
-    console.log('[ZephyrClient] Starting tool registration...');
+  registerTools(
+    register: RegisterToolsFunction,
+    _getInput: GetInputFunction,
+  ): void {
+    console.log("[ZephyrClient] Starting tool registration...");
+    const tools: ZephyrTool[] = [new GetProjects(this.apiClient)];
 
-    register(
-      {
-        title: "Get Projects",
-        summary: "Get details of projects in Zephyr",
-        parameters: [
-          {
-            name: "maxResults",
-            type: z.number()
-              .min(1)
-              .max(1000)
-              .describe(`
-                Specifies the maximum number of results to return in a single call. The default value is 10, and the maximum value that can be requested is 1000.
+    tools.forEach((tool) => {
+      console.log(
+        `[ZephyrClient] Registering tool: ${tool.specification.title}`,
+      );
+      register(tool.specification, tool.handle);
+    });
 
-                Note that the server may enforce a lower limit than requested, depending on resource availability or other internal constraints.
-                If this happens, the result set may be truncated. Always check the maxResults value in the response to confirm how many results were actually returned.
-              `),
-            required: false,
-          },
-          {
-            name: "startAt",
-            type: z.number()
-              .min(0)
-              .max(1000000)
-              .describe(`
-                Zero-indexed starting position used to paginate through results. Defaults to 0.
-              `),
-            required: false,
-          },
-        ],
-      },
-      async (args, _extra) => {
-        const response = await this.projectTools.getProjects(args.maxResults, args.startAt);
-        return {
-          content: [{ type: "text", text: JSON.stringify(response) }],
-        };
-      }
-    );
-
-    console.log('[ZephyrClient] Finished tool registration...');
+    console.log("[ZephyrClient] Finished tool registration...");
   }
-
 }
