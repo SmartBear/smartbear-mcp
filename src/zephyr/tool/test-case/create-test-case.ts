b2c4b8199d3f49d48b90c35344cdebd2400bbc7d
adrien.avenia <adrien.avenia@smartbear.com>
2026-02-03 16:14:24 +0100

    TM4J-14623: Add example using custom fields

--- src/zephyr/tool/test-case/create-test-case.ts
+++ src/zephyr/tool/test-case/create-test-case.ts
@@ -1,5 +1,5 @@
 import type { ToolCallback } from "@modelcontextprotocol/sdk/server/mcp.js";
-import type { ZodRawShape } from "zod";
+import { custom, type ZodRawShape } from "zod";
 import { Tool } from "../../../common/tools";
 import type { ToolParams } from "../../../common/types";
 import type { ZephyrClient } from "../../client";
@@ -35,6 +35,20 @@ export class CreateTestCase extends Tool<ZephyrClient> {
           objective: "This test case is automated and created via MCP",
           labels: ["automated", "mcp"],
           priorityName: "High",
+        },
+        expectedOutput: "The newly created Test Case with its details and key",
+      },
+      {
+        description:
+          "Create a Test Case for verifying strength of the axial pump with custom field 'Axial pump strength' having value '5' in project SA",
+        parameters: {
+          projectKey: "SA",
+          name: "Check axial pump strength",
+          objective:
+            "Make sure the axial pump operates at the required strength",
+          customFields: {
+            "Axial pump strength": 5,
+          },
         },
         expectedOutput: "The newly created Test Case with its details and key",
       },
