f84251f5af485a3fea2490e3ba99b2a08fe15941
jakub.bartus <jakub.bartus@smartbear.com>
2026-03-13 10:23:58 +0100

    Added a new tool get-test-steps for test execution and updated rest-api-schemas for correctly retrieve.

--- src/zephyr/client.ts
+++ src/zephyr/client.ts
@@ -31,6 +31,7 @@ import { UpdateTestCycle } from "./tool/test-cycle/update-test-cycle.ts";
 import { CreateTestExecution } from "./tool/test-execution/create-test-execution";
 import { GetTestExecution } from "./tool/test-execution/get-test-execution";
 import { GetTestExecutions } from "./tool/test-execution/get-test-executions";
+import { GetTestExecutionSteps } from "./tool/test-execution/get-test-steps.ts";
 import { UpdateTestExecution } from "./tool/test-execution/update-test-execution";
 
 const BASE_URL_DEFAULT = "https://api.zephyrscale.smartbear.com/v2";
@@ -104,6 +105,7 @@ export class ZephyrClient implements Client {
       new GetTestCaseSteps(this),
       new GetIssueLinkTestCases(this),
       new CreateTestCycleWebLink(this),
+      new GetTestExecutionSteps(this),
     ];
 
     for (const tool of tools) {
