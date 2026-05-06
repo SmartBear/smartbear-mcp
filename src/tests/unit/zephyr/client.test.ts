cdbb57563159dc13246ceed1cc8c75ba6c42588d
Assis <assisblf@gmail.com>
2026-05-06 12:18:56 -0300

    feat: exclude src/tests from changelog check

--- src/tests/unit/zephyr/client.test.ts
+++ src/tests/unit/zephyr/client.test.ts
@@ -3,7 +3,7 @@ import { ZephyrClient } from "../../../zephyr/client";
 import { ApiClient } from "../../../zephyr/common/api-client";
 
 describe("ZephyrClient", () => {
-  it("should set name and prefix", () => {
+  it("should set name and prefixs", () => {
     const client = new ZephyrClient();
     expect(client.name).toBe("Zephyr");
     expect(client.toolPrefix).toBe("zephyr");
