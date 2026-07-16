c5b07e37148a63968b9c500a7d1bac48aba1cca2
Kevin Rohan Vaz <kevin.rohanvaz@smartbear.com>
2026-07-16 10:46:55 +0530

    chore(pactflow): update user agent

--- src/pactflow/client.ts
+++ src/pactflow/client.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-import { USER_AGENT } from "../common/info";
+import { getUserAgent } from "../common/info";
 import {
   isSamplingPolyfillResult,
   type SamplingPolyfillResult,
@@ -285,13 +285,6 @@ export class PactflowClient implements Client {
       contextToken = contextToken[0];
     }
 
-    const clientInfo = this._server?.getClientInfo();
-    const sourceApplicationHeader: Record<string, string> = {
-      SOURCE_APPLICATION: clientInfo
-        ? `${clientInfo.name}/${clientInfo.version}`
-        : "unknown",
-    };
-
     if (contextToken) {
       let authHeader = contextToken;
       if (
@@ -304,8 +297,7 @@ export class PactflowClient implements Client {
       return {
         Authorization: authHeader,
         "Content-Type": "application/json",
-        "User-Agent": USER_AGENT,
-        ...sourceApplicationHeader,
+        "User-Agent": getUserAgent(),
       };
     }
 
@@ -321,16 +313,14 @@ export class PactflowClient implements Client {
       return {
         Authorization: authHeader,
         "Content-Type": "application/json",
-        "User-Agent": USER_AGENT,
-        ...sourceApplicationHeader,
+        "User-Agent": getUserAgent(),
       };
     } else if (this.username && this.password) {
       const authString = `${this.username}:${this.password}`;
       return {
         Authorization: `Basic ${Buffer.from(authString).toString("base64")}`,
         "Content-Type": "application/json",
-        "User-Agent": USER_AGENT,
-        ...sourceApplicationHeader,
+        "User-Agent": getUserAgent(),
       };
     }
     return undefined;
