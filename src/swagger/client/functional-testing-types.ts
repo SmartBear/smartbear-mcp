163e0e74b46fdb592acf8ade4258d11fde80fec8
Bartosz Kosicki <bartosz.kosicki@smartbear.com>
2026-08-05 14:05:45 +0200

    fix(functional-testing): make url required in Create Suite response
    The Functional Testing API always returns url when creating a suite,
    so the schema shouldn't mark it optional.

--- src/swagger/client/functional-testing-types.ts
+++ src/swagger/client/functional-testing-types.ts
@@ -114,8 +114,7 @@ export const CreateFunctionalTestingSuiteResponseSchema = z.object({
   slug: z.string().describe("Slug of the newly created suite"),
   url: z
     .string()
-    .describe("Link to the created suite in Swagger Functional Testing UI")
-    .optional(),
+    .describe("Link to the created suite in Swagger Functional Testing UI"),
 });
 
 export type CreateFunctionalTestingSuiteResponse = z.infer<
